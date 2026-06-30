c37154517fd9ef93cbf75326b3cc01c2ac50847c
kks600-sketch <kks600@gmail.com>
2026-06-30 10:23:29 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,44 @@
+// % color="#E11D48" icon="\uf085" block="ควบคุมมอเตอร์"
+namespace customMotor {
+
+    export enum MotorDir {
+        // % block="เดินหน้า"
+        Forward,
+        // % block="ถอยหลัง"
+        Backward
+    }
+
+    /**
+     * สั่งงานมอเตอร์ให้เคลื่อนที่ เดินหน้า หรือ ถอยหลัง
+     * @param direction ทิศทางเคลื่อนที่
+     * @param speed ความเร็ว 0-100 %, eg: 60
+     */
+    // % block="สั่งรถ %direction ความเร็ว %speed %%"
+    // % speed.min=0 speed.max=100
+    export function move(direction: MotorDir, speed: number): void {
+        let pwnVal = pins.map(speed, 0, 100, 0, 1023)
+        if (direction === MotorDir.Forward) {
+            pins.digitalWritePin(DigitalPin.P13, 1)
+            pins.analogWritePin(AnalogPin.P14, pwnVal)
+            pins.digitalWritePin(DigitalPin.P15, 1)
+            pins.analogWritePin(AnalogPin.P16, pwnVal)
+        } else if (direction === MotorDir.Backward) {
+            pins.digitalWritePin(DigitalPin.P13, 0)
+            pins.analogWritePin(AnalogPin.P14, pwnVal)
+            pins.digitalWritePin(DigitalPin.P15, 0)
+            pins.analogWritePin(AnalogPin.P16, pwnVal)
+        }
+    }
+
+    /**
+     * สั่งให้มอเตอร์รถหยุดวิ่งทันที
+     */
+    // % block="หยุดรถ"
+    export function stop(): void {
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+    }
+}
 
